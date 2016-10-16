6e5061d72e2e0ef2732775cc629d54f414bc3d36
Valeri Dinov <valersd@abv.bg>
2016-10-16 23:05:52 +0300

    Remove App_Code

---
+++ CarService.Web/Infrastructure/HtmlHelpers/ShowHideElement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarService.Web.Infrastructure.HtmlHelpers
+{
+    public static class ShowHideElement
+    {
+        public static MvcHtmlString ShowHide(string id, bool showOnInitialization, string additionalCssClass = null, string textForShow = "[ Show ]", string textForHide = "[ Hide ]")
+        {
+            string cssClass = "show-hide-link";
+            if (!string.IsNullOrEmpty(additionalCssClass))
+            {
+                cssClass += " " + additionalCssClass;
+            }
+
+            TagBuilder container = new TagBuilder("a");
+            container.AddCssClass(cssClass);
+            container.MergeAttribute("id", id);
+            container.MergeAttribute("href", "javascript:void(0);");
+
+            TagBuilder forShow = new TagBuilder("em");
+            forShow.InnerHtml = textForShow;
+            TagBuilder forHide = new TagBuilder("em");
+            forHide.InnerHtml = textForHide;
+
+            if (showOnInitialization)
+            {
+                forShow.MergeAttribute("style", "display:none");
+            }
+            else
+            {
+                forHide.MergeAttribute("style", "display:none");
+            }
+
+            container.InnerHtml += forShow;
+            container.InnerHtml += forHide;
+
+            return MvcHtmlString.Create(container.ToString());
+        }
+    }
+}
